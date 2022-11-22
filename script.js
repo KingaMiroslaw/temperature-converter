85aa5315c434ad2f152d5ab0de8607cf0dda026b
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2022-11-22 10:36:20 +0100

    add temperature converter project files

---
+++ script.js
@@ -0,0 +1,52 @@
+const converter = document.querySelector("#converter");
+const result = document.querySelector(".result");
+const convBtn = document.querySelector(".conv");
+const resetBtn = document.querySelector(".reset");
+const changeBtn = document.querySelector(".change");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+
+const swap = () => {
+  if (one.textContent === "°C") {
+    one.textContent = "°F";
+    two.textContent = "°C";
+    result.textContent = "";
+  } else {
+    one.textContent = "°C";
+    two.textContent = "°F";
+    result.textContent = "";
+  }
+};
+
+const celToFahr = () => {
+  const fahrenheit = converter.value * 1.8 + 32;
+  result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`;
+  converter.value = "";
+};
+
+const fahrToCel = () => {
+  const celsius = (converter.value - 32) / 1.8;
+  result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`;
+  converter.value = "";
+};
+
+const conversion = () => {
+  if (converter.value !== "") {
+    if (one.textContent === "°C") {
+      celToFahr();
+    } else {
+      fahrToCel();
+    }
+  } else {
+    result.textContent = "You must enter some value!";
+  }
+};
+
+const reset = () => {
+  converter.value = "";
+  result.textContent = "";
+};
+
+changeBtn.addEventListener("click", swap);
+convBtn.addEventListener("click", conversion);
+resetBtn.addEventListener("click", reset);
